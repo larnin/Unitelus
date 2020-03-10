9c89b256a016f62255fdb65be7bfb524dfef0419
larnin <nicolas-donald.laurent@laposte.net>
2020-03-10 03:08:33 +0100

    Change chunk axis y <=> z and make layer bigger

--- Assets/Scripts/World/Chunk.cs
+++ Assets/Scripts/World/Chunk.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 
 public class ChunkLayer
 {
-    BlockData[] m_blocks = new BlockData[Chunk.chunkSize * Chunk.chunkSize];
+    BlockData[] m_blocks = new BlockData[Chunk.chunkSize * Chunk.chunkSize * Chunk.chunkSize];
     int m_blockNb = 0;
 
     public int GetBlockNb()
@@ -20,19 +20,19 @@ public class ChunkLayer
         return m_blockNb <= 0;
     }
 
-    public BlockData GetBlock(int x, int y)
+    public BlockData GetBlock(int x, int y, int z)
     {
-        Debug.Assert(x >= 0 && x < Chunk.chunkSize && y >= 0 && y < Chunk.chunkSize);
+        Debug.Assert(x >= 0 && x < Chunk.chunkSize && y >= 0 && y < Chunk.chunkSize && z >= 0 && z < Chunk.chunkSize);
 
-        return m_blocks[PosToIndex(x, y)];
+        return m_blocks[PosToIndex(x, y, z)];
     }
 
-    public void SetBlock(int x, int y, BlockData bloc)
+    public void SetBlock(int x, int y, int z, BlockData bloc)
     {
-        Debug.Assert(x >= 0 && x < Chunk.chunkSize && y >= 0 && y < Chunk.chunkSize);
+        Debug.Assert(x >= 0 && x < Chunk.chunkSize && y >= 0 && y < Chunk.chunkSize && z >= 0 && z < Chunk.chunkSize);
 
         BlockData empty = BlockData.GetDefault();
-        int index = PosToIndex(x, y);
+        int index = PosToIndex(x, y, z);
 
         if (m_blocks[index] != empty)
             m_blockNb--;
@@ -40,12 +40,12 @@ public class ChunkLayer
         if (bloc != empty)
             m_blockNb++;
 
-        m_blocks[PosToIndex(x, y)] = bloc;
+        m_blocks[index] = bloc;
     }
 
-    int PosToIndex(int x, int y)
+    int PosToIndex(int x, int y, int z)
     {
-        return x * Chunk.chunkSize + y;
+        return (x * Chunk.chunkSize + y) * Chunk.chunkSize + z;
     }
 }
 
@@ -55,77 +55,86 @@ public class Chunk
 
     World m_world = null;
     int m_x = 0;
-    int m_y = 0;
+    int m_z = 0;
     float m_updateTime = 0;
 
     Dictionary<int, ChunkLayer> m_layers = new Dictionary<int, ChunkLayer>();
 
     public World world { get { return m_world; } }
     public int x { get { return m_x; } }
-    public int y { get { return m_y; } }
+    public int z { get { return m_z; } }
     public float updateTime { get { return m_updateTime; } }
 
-    public Chunk(World world, int x, int y)
+    public Chunk(World world, int x, int z)
     {
         m_world = world;
         m_x = x;
-        m_y = y;
+        m_z = z;
     }
 
     public BlockData GetBlock(int x, int y, int z)
     {
-        if(m_layers.ContainsKey(z))
+        int layer, block;
+        HeightToLayerAndBlock(y, out layer, out block);
+
+        if(m_layers.ContainsKey(layer))
         {
-            return m_layers[z].GetBlock(x, y);
+            return m_layers[layer].GetBlock(x, block, z);
         }
         return BlockData.GetDefault();
     }
 
     public void SetBlock(int x, int y, int z, BlockData block)
     {
-        if(m_layers.ContainsKey(z))
-        {
-            var layer = m_layers[z];
+        int l, b;
+        HeightToLayerAndBlock(y, out l, out b);
 
-            layer.SetBlock(x, y, block);
+        if (m_layers.ContainsKey(l))
+        {
+            var layer = m_layers[l];
+
+            layer.SetBlock(x, b, z, block);
 
             if(layer.IsEmpty())
-                m_layers.Remove(z);
+                m_layers.Remove(l);
         }
         else
         {
             if (block == BlockData.GetDefault())
                 return;
             var layer = new ChunkLayer();
-            layer.SetBlock(x, y, block);
-            m_layers.Add(z, layer);
+            layer.SetBlock(x, b, z, block);
+            m_layers.Add(l, layer);
         }
 
         m_updateTime = Time.time;
     }
 
-    public int GetHeight(int x, int y)
+    public ChunkLayer GetLayerAt(int y)
     {
-        int z = int.MinValue;
-        foreach(var l in m_layers)
-        {
-            if (l.Key > z && l.Value.GetBlock(x, y) != BlockData.GetDefault())
-                z = l.Key;
-        }
-
-        return z;
+        return GetLayer(HeightToLayer(y));
     }
 
-    public ChunkLayer GetLayer(int z)
+    public bool HaveLayerAt(int y)
     {
-        if (m_layers.ContainsKey(z))
-            return m_layers[z];
+        return HaveLayer(HeightToLayer(y));
+    }
+
+    public ChunkLayer GetLayer(int layer)
+    {
+        if (m_layers.ContainsKey(layer))
+            return m_layers[layer];
         return null;
     }
 
-    public bool HaveLayer(int z)
+    public bool HaveLayer(int layer)
     {
-        return m_layers.ContainsKey(z);
+        return m_layers.ContainsKey(layer);
+    }
+
+    public bool HaveLayer()
+    {
+        return m_layers.Count > 0;
     }
 
     public int[] GetLayers()
@@ -140,5 +149,108 @@ public class Chunk
         }
 
         return layers;
+    }
+
+    public int GetTopLayerIndex()
+    {
+        Debug.Assert(HaveLayer());
+
+        int height = int.MinValue;
+
+        foreach(var l in m_layers)
+            height = Mathf.Max(l.Key, height);
+
+        return height;
+    }
+
+    public int GetTopBlockHeight(int x, int z)
+    {
+        Debug.Assert(HaveLayer());
+
+        if (!HaveLayer())
+            return int.MinValue;
+
+        int layerIndex = GetTopLayerIndex();
+        var layer = m_layers[layerIndex];
+
+        int i = 0;
+        for (i = chunkSize - 1; i >= 0; i--)
+        {
+            if (layer.GetBlock(x, i, z) != BlockData.GetDefault())
+                break;
+        }
+
+        Debug.Assert(i >= 0);
+
+        return LayerToHeight(layerIndex, i);
+    }
+
+    public int GetBottomLayerIndex()
+    {
+        Debug.Assert(HaveLayer());
+
+        int height = int.MaxValue;
+
+        foreach (var l in m_layers)
+            height = Mathf.Min(l.Key, height);
+
+        return height;
+    }
+
+    public int GetBottomBlockHeight(int x, int z)
+    {
+        Debug.Assert(HaveLayer());
+
+        if (!HaveLayer())
+            return int.MaxValue;
+
+        int layerIndex = GetBottomLayerIndex();
+        var layer = m_layers[layerIndex];
+
+        int i = 0;
+        for (i = 0; i < chunkSize; i++)
+        {
+            if (layer.GetBlock(x, i, z) != BlockData.GetDefault())
+                break;
+        }
+
+        Debug.Assert(i < chunkSize);
+
+        return LayerToHeight(layerIndex, i);
+    }
+
+    public int HeightToLayer(int height)
+    {
+        if (height < 0)
+            return (height - chunkSize + 1) / chunkSize;
+        return height / chunkSize;
+    }
+    
+    public int HeightToBlockInLayer(int height)
+    {
+        if (height < 0)
+            return height % chunkSize + chunkSize - 1;
+        return height % chunkSize;
+    }
+
+    public void HeightToLayerAndBlock(int height, out int layer, out int block)
+    {
+        if(height < 0)
+        {
+            layer = (height - chunkSize + 1) / chunkSize;
+            block = height % chunkSize + chunkSize - 1;
+        }
+        else
+        {
+            layer = height / chunkSize;
+            block = height % chunkSize;
+        }
+    }
+
+    public int LayerToHeight(int layer, int block = 0)
+    {
+        Debug.Assert(block >= 0 && block < chunkSize);
+
+        return layer * chunkSize + block;
     }
 }
