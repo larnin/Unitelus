f7b73b04a2135d58d5d2338a41681c67c1c28bf9
larnin <nicolas-donald.laurent@laposte.net>
2020-03-12 23:51:10 +0100

    World mesh vertices definitions

---
+++ Assets/Scripts/World/Renderer/MeshParams.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity.Collections;
+using UnityEngine;
+
+public class MeshParamData<T> where T : struct
+{
+    public const int maxVertexSize = ushort.MaxValue - 5;
+
+    public NativeArray<T> vertices;
+    public int verticesSize = 0;
+    public ushort[] indexes = null;
+    public int indexesSize = 0;
+
+    public bool CanAllocate(int vertexSize)
+    {
+        return vertexSize + verticesSize <= maxVertexSize;
+    }
+}
+
+public class MeshParams<T> where T : struct
+{
+    const int allocSize = 1000;
+
+    Dictionary<Material, List<MeshParamData<T>>> m_data = new Dictionary<Material, List<MeshParamData<T>>>();
+
+    //used to return stuff without new GC alloc
+    MeshParamData<T> m_returnData = new MeshParamData<T>();
+
+    public MeshParamData<T> Allocate(int vertexSize, int indexSize, Material material)
+    {
+        Debug.Assert(vertexSize <= MeshParamData<T>.maxVertexSize);
+
+        List<MeshParamData<T>> data = null;
+        m_data.TryGetValue(material, out data);
+
+        if(data == null)
+        {
+            data = new List<MeshParamData<T>>();
+            m_data[material] = data;
+            data.Add(new MeshParamData<T>());
+            AllocateVerticesArray(data[0], vertexSize);
+            AllocateIndexArray(data[0], indexSize);
+        }
+
+        var element = data[data.Count - 1];
+
+        if(!element.CanAllocate(vertexSize))
+        {
+            var newData = new MeshParamData<T>();
+            data.Add(newData);
+            AllocateVerticesArray(newData, vertexSize);
+            AllocateIndexArray(newData, indexSize);
+            element = newData;
+        }
+
+        if (element.verticesSize + vertexSize > element.vertices.Length)
+            IncreaseVerticesArray(element, vertexSize);
+        if (element.indexesSize + indexSize > element.indexes.Length)
+            IncreaseIndexArray(element, indexSize);
+
+        m_returnData.vertices = element.vertices;
+        m_returnData.verticesSize = element.verticesSize;
+        m_returnData.indexes = element.indexes;
+        m_returnData.indexesSize = element.indexesSize;
+
+        element.verticesSize += vertexSize;
+        element.indexesSize += indexSize;
+
+        return m_returnData;
+    }
+
+    void AllocateVerticesArray(MeshParamData<T> data, int addVertices)
+    {
+        Debug.Assert(!data.vertices.IsCreated);
+        Debug.Assert(addVertices <= MeshParamData<T>.maxVertexSize);
+
+        int newSize = addVertices + allocSize;
+        if (newSize > MeshParamData<T>.maxVertexSize)
+            newSize = MeshParamData<T>.maxVertexSize;
+
+        data.vertices = new NativeArray<T>(newSize, Allocator.Persistent);
+    }
+
+    void IncreaseVerticesArray(MeshParamData<T> data, int addVertices)
+    {
+        Debug.Assert(data.vertices.IsCreated);
+        Debug.Assert(data.vertices.Length + addVertices <= MeshParamData<T>.maxVertexSize);
+
+        int newSize = data.vertices.Length + addVertices + allocSize;
+        if (newSize > MeshParamData<T>.maxVertexSize)
+            newSize = MeshParamData<T>.maxVertexSize;
+
+        var newArray = new NativeArray<T>(newSize, Allocator.Persistent);
+
+        for (int i = 0; i < data.verticesSize; i++)
+            newArray[i] = data.vertices[i];
+        data.vertices.Dispose();
+        data.vertices = newArray;
+    }
+
+    void AllocateIndexArray(MeshParamData<T> data, int addIndexes)
+    {
+        Debug.Assert(data.indexes == null);
+
+        data.indexes = new ushort[addIndexes + allocSize];
+    }
+
+    void IncreaseIndexArray(MeshParamData<T> data, int addIndexes)
+    {
+        Debug.Assert(data.indexes != null);
+
+        int newSize = data.indexes.Length + addIndexes + allocSize;
+
+        var newIndex = new ushort[newSize];
+
+        for (int i = 0; i < data.indexesSize; i++)
+            newIndex[i] = data.indexes[i];
+        data.indexes = newIndex;
+    }
+}
---
+++ Assets/Scripts/World/Renderer/WorldVertexDefinition.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public struct WorldVertexDefinition
+{
+    public Vector3 pos;
+    public Vector3 normal;
+    public Vector4 tangent;
+    public Vector2 uv;
+    public Color color;
+}
+
+public static class MeshEx
+{
+    static void SetWorldMeshParams(Mesh mesh, int vertexNb, int indexNb)
+    {
+        var layout = new[]
+        {
+            new VertexAttributeDescriptor(VertexAttribute.Position, VertexAttributeFormat.Float32, 3),
+            new VertexAttributeDescriptor(VertexAttribute.Normal, VertexAttributeFormat.Float32, 3),
+            new VertexAttributeDescriptor(VertexAttribute.Tangent, VertexAttributeFormat.Float32, 4),
+            new VertexAttributeDescriptor(VertexAttribute.TexCoord0, VertexAttributeFormat.Float32, 2),
+            new VertexAttributeDescriptor(VertexAttribute.Color, VertexAttributeFormat.Float32, 4)
+        };
+
+        mesh.SetVertexBufferParams(vertexNb, layout);
+
+        mesh.SetIndexBufferParams(indexNb, IndexFormat.UInt16);
+    }
+}
