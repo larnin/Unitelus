f451def5172c8d6ed3eb8f45b4be9a3157c8fa61
larnin <nicolas-donald.laurent@laposte.net>
2021-07-29 14:24:29 +0200

    Improve event to allow to target a specific entity

--- Assets/Scripts/Events/Event.cs
+++ Assets/Scripts/Events/Event.cs
@@ -1,44 +1,98 @@
 ﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
 
 class Event<T>
 {
 	public class Subscriber : IEventSubscriber
 	{
-		private Action<T> func_;
+		private Action<T> m_func;
 
 		public Subscriber(Action<T> func)
 		{
-			func_ = func;
+            m_func = func;
 		}
 
 		void IEventSubscriber.Subscribe()
 		{
-			Event<T>.Subscribe(func_);
+			Event<T>.Subscribe(m_func);
 		}
 
 		void IEventSubscriber.Unsubscribe()
 		{
-			Event<T>.Unsubscribe(func_);
+			Event<T>.Unsubscribe(m_func);
 		}
 	}
 
-	private static event Action<T> _event;
+    private static event Action<T> m_event;
 
 	public static void Subscribe(Action<T> func)
 	{
-		_event += func;
+        m_event += func;
 	}
 
 	public static void Unsubscribe(Action<T> func)
 	{
-		_event -= func;
+        m_event -= func;
 	}
 
 	public static void Broadcast(T data)
 	{
-		if (_event != null)
+		if (m_event != null)
 		{
-			_event(data);
+            m_event(data);
 		}
 	}
+
+    //------------------------------------------------------
+    public class LocalSubscriber : IEventSubscriber
+    {
+        private Action<T> m_func;
+        private GameObject m_obj;
+
+        public LocalSubscriber(Action<T> func, GameObject obj)
+        {
+            m_func = func;
+            m_obj = obj;
+        }
+
+        void IEventSubscriber.Subscribe()
+        {
+            Event<T>.Subscribe(m_func, m_obj);
+        }
+
+        void IEventSubscriber.Unsubscribe()
+        {
+            Event<T>.Unsubscribe(m_func, m_obj);
+        }
+    }
+
+    private static Dictionary<GameObject, Action<T>> m_events = new Dictionary<GameObject, Action<T>>();
+
+    public static void Subscribe(Action<T> func, GameObject obj)
+    {
+        if (m_events.ContainsKey(obj))
+            m_events[obj] += func;
+        else m_events.Add(obj, func);
+    }
+
+    public static void Unsubscribe(Action<T> func, GameObject obj)
+    {
+        if (m_events.ContainsKey(obj))
+            m_events[obj] -= func;
+    }
+
+    public static void Broadcast(T data, GameObject obj, bool propagateToChild = false)
+    {
+        Action<T> func;
+        if (m_events.TryGetValue(obj, out func) && func != null)
+            func(data);
+
+        if(propagateToChild)
+        {
+            var t = obj.transform;
+            for(int i = 0; i < t.childCount; i++)
+                Broadcast(data, t.GetChild(i).gameObject, true);
+        }
+    }
 }
